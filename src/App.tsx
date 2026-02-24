d9543532250013f46bffc86776daa66ec0d6f5c0
[dyad] <patrikmak.student@gmail.com>
2026-02-23 23:10:52 -0300

    [dyad] Corrigindo erro de useState e conflito de versões - wrote 1 file(s)

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import React from 'react';
 import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
